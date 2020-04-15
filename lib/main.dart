17811bcd816291e919a896f52807283219a25074
Eduardo Britto <dubritto93@gmail.com>
2020-04-15 11:34:58 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
+import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'models/cart_model.dart';
+import 'models/user_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model){
+          return ScopedModel<CartModel>(
+            model: CartModel(model),
+            child: MaterialApp(
+                title: 'Flutters Clothing',
+                theme: ThemeData(
+                    primarySwatch: Colors.blue,
+                    primaryColor: Color.fromARGB(255, 4, 125, 141)
+                ),
+                debugShowCheckedModeBanner: false,
+                home: HomeScreen()
+            ),
+
+          );
+        }
+      ),
+    );
+
+  }
+}
+
+
